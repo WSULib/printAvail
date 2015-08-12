96bcd03a0228ba13a7dd5b4b6cd5dfd305e14c28
Graham Hukill <ghukill@gmail.com>
2015-08-12 12:21:53 +1000

    testing

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Small SQLlite powered application for internal monitoring of printer availability
 
 * directory and printAvail.db must be owned by www-data (index.php can be other user)
+* printers are an array in config.php
+* manage.php page
+** contains username and password manage page
---
+++ debug.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="keywords" content="Wayne State University, WSU, Library System, Libraries" />
+        <meta name="description" content="The online resources and services of the Wayne State University Libraries" />
+        <meta name="author" content="libwebmaster@wayne.edu" />
+        <meta name="Copyright" content="Copyright (c) <?php echo(date('Y')); ?> Wayne State University" />
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="icon" href="/inc/img/favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" href="/ico/style.css">
+  <link href="//library.wayne.edu/inc/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="/pattern-lib/css/style.css">
+
+        <title>Wayne State University Libraries : About</title>
+
+        <script src="/pattern-lib/js/jquery.min.js"></script>
+    <script src="/pattern-lib/js/bootstrap.min.js"></script>
+    <script src="/pattern-lib/js/main.js"></script>
+</head>
+<body>
+        <div class="page" id="wrap">
+                <?php include($_SERVER['DOCUMENT_ROOT'].'inc/header.php'); ?>
+                <div id="main" class="container">
+                    <p>Hello World.</p>
+
+
+                </div>
+
+        </div>
+</body>
+</html>`
